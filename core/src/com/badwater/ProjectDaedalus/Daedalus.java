203a83dc4896f91e966e2810b527f64af0697054
Robert Straitt <rstr5105@gmail.com>
2014-07-14 21:30:06 -0700

    Imported the GDX Sample, started converting project to GDX from AWT. Should not be too hard, most implemented things have nothing to do with AWT/SWING.  Guess I'm sticking to Java, it seems to be where my experience lies.  JAVA.  Lovely.
    Signed-off-by: Robert Straitt <rstr5105@gmail.com>

--- core/src/com/badwater/ProjectDaedalus/Daedalus.java
+++ core/src/com/badwater/ProjectDaedalus/Daedalus.java
@@ -6,19 +6,22 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badwater.ProjectDaedalus.World.*;
 public class Daedalus extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		World world = new World(10,10); 
+		world.print();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1,0, 1);
+		Gdx.gl.glClearColor(1, 0 ,0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
